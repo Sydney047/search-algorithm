a12af57bbca354df702a0ead466777c42dc28b30
Sydney <meshcmoose@gmail.com>
2025-10-21 14:13:04 +0530

    EDIT .

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const search = document.querySelector("#search");
 const param = document.querySelector("#param");
 let results = "";
 
-button.addEventListener("click", buttonClicked)
-
 let names = ["Michael Jordan:Basketball","Serena Williams:Tennis","Usain Bolt:Track & Field","Messi:Football","Ronaldo:Football",
     "Tom Brady:American Football","Muhammad Ali:Boxing", "Tiger Woods:Golf"];
 
@@ -23,20 +21,22 @@ function checksSearch(){
 
         if (toCheck === checker[0]){
             if ((toCheck === "Messi") || (toCheck==="Ronaldo")){
-                let results = `${checker[0]} is one of two GOATS of ${checker[1]}`;
+                results = `${checker[0]} is one of two GOATS of ${checker[1]}`;
                 return results;
             }else {
-            let results = `${checker[0]} is the GOAT of ${checker[1]}`;
+            results = `${checker[0]} is the GOAT of ${checker[1]}`;
             return results;
             }
         } else if (toCheck === checker[1]) {
             if (toCheck === "Football"){
-                let results = `${toCheck} is dominated by Messi & Ronaldo`;
+                results = `${toCheck} is dominated by Messi & Ronaldo`;
                 return results;
             } else {
-                let results = `${checker[1]} is dominated by ${checker[0]}`;
+                results = `${checker[1]} is dominated by ${checker[0]}`;
                 return results;
             }
         }
     }
 }
+
+button.addEventListener("click", buttonClicked);
